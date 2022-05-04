16f7e089e1a48743af36fcda8a3a804dd33e704f
Ibrahim Mohamed <ibrah.salama@outlook.com>
2022-05-04 20:52:18 +0200

    Adding project lombok and logger

--- src/main/java/guru/springframework/spring5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/UnitOfMeasure.java
@@ -1,28 +1,18 @@
 package guru.springframework.spring5recipeapp.domain;
 
-import javax.persistence.*;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
 @Entity
 public class UnitOfMeasure  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
     private String description;
 
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String uom) {
-        this.description = uom;
-    }
 }
